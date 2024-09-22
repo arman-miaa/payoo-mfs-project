d74c61c495d8ac46ee0cecfc1ecca643adfa8c58
Arman <mdarman365506@gmail.com>
2024-09-22 09:35:52 +0600

    added all section for loging page

--- js/loging.js
+++ js/loging.js
@@ -6,9 +6,19 @@ document.getElementById("button-login")
     .addEventListener("click", function (event) {
         // step - 2 prevent default behavior (prevent reloding browser)
         event.preventDefault();
-        console.log("login button clicked");
         
-        // step -3 get the phone number
-        const phoneNumber = document.getElementById("phone-number");
-        console.log(phoneNumber.value);
+        // step -3 get the phone and pin number
+        const phoneNumber = document.getElementById("phone-number").value;
+        const pinNumber = document.getElementById("pin-number").value;
+        console.log(phoneNumber, pinNumber);
+        
+        // step 4: bad way to validate 
+        // this is temporay, You should do like this
+        if (phoneNumber === '5' && pinNumber === '1234') {
+            console.log('you are logged in');
+            // step 5: allow user to use the website
+        }
+        else {
+            alert('Wrong phone number or  pin')
+        }
 });
